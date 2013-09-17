034da088aa4e7debbedde32c2aa0d730dc642a77
Carlos Beltran-Gonzalez <cbeltran@libero.it>
2013-09-17 17:02:03 +0200

    more vectorization

--- mscrdist.cpp
+++ mscrdist.cpp
@@ -406,33 +406,29 @@ int main( int argc, char** argv)
         return -1;
     }
 
-    MSCR mscr1;
-    MSCR mscr2;
     vector<MSCR> mscr_vec(2);
 
     Mat blobPos_1_;
     readmat("./gtdata/mscrmvec_00001.txt", blobPos_1_);
     Mat blobPos_1(blobPos_1_,cv::Range(2,3),cv::Range(0,39));
-    //mscr1.blobPos = blobPos_1.clone();
     mscr_vec.at(0).blobPos = blobPos_1.clone();
     
     Mat blobPos_2_;
     readmat("./gtdata/mscrmvec_00002.txt", blobPos_2_);
     Mat blobPos_2(blobPos_2_,cv::Range(2,3),cv::Range(0,54));
-    //mscr2.blobPos = blobPos_2.clone();
     mscr_vec.at(1).blobPos = blobPos_2.clone();
 
-    readmat("./gtdata/mscrpvec_00001.txt", mscr1.blobColor);
-    rgb2lab(mscr1.blobColor);
+    readmat("./gtdata/mscrpvec_00001.txt", mscr_vec.at(0).blobColor);
+    rgb2lab(mscr_vec.at(0).blobColor);
 
-    readmat("./gtdata/mscrpvec_00002.txt", mscr2.blobColor);
-    rgb2lab(mscr2.blobColor);
+    readmat("./gtdata/mscrpvec_00002.txt", mscr_vec.at(1).blobColor);
+    rgb2lab(mscr_vec.at(1).blobColor);
 
     //dist(blobPos_1,blobPos_2, blobCol_1, blobCol_2, 0.4);
     dist(mscr_vec.at(0).blobPos,
          mscr_vec.at(1).blobPos, 
-         mscr1.blobColor, 
-         mscr2.blobColor, 0.4);
+         mscr_vec.at(0).blobColor, 
+         mscr_vec.at(1).blobColor,0.4);
 
     namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
     imshow("Display Image", image);
